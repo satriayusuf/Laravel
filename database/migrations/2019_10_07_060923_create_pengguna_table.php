161a0d0c45048ec91871a9386161f06b3e54ddef
Muhammad Azickri Al Islam <force.sektor@gmail.com>
2019-10-07 13:19:44 +0700

    Laravel
    Laravel

---
+++ database/migrations/2019_10_07_060923_create_pengguna_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePenggunaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengguna', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->string('no-telepon');
+            $table->text('alamat');
+            $table->text('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengguna');
+    }
+}
