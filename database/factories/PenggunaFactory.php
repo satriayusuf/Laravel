04f3104a5780a24b19382d5f01e8d1da113f079b
Muhammad Azickri Al Islam <force.sektor@gmail.com>
2019-10-09 15:13:58 +0700

    Factory

--- database/factories/PenggunaFactory.php
+++ database/factories/PenggunaFactory.php
@@ -7,10 +7,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Pengguna::class, function (Faker $faker) {
     return [
-        'nama' => $faker->nama,
-        'no_telepon' => $faker->no_telepon,
-        'email' => $faker->email,
-        'alamat' => $faker->alamat,
-        'created_at' => $faker->created_at
+        'created_at' => $faker->created_at,
     ];
 });
